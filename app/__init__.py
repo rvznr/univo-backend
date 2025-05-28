cfbd0d6bc6fc672912fac6d82b65fe9137a56568
Ravza <ravzasisiknur@gmail.com>
2025-05-28 15:58:44 +0300

    A

--- app/__init__.py
+++ app/__init__.py
@@ -26,7 +26,9 @@ def create_app():
         app,
         origins=[
             "http://localhost:3000",
-            "https://frontend-univo.vercel.app"
+            "https://frontend-univo.vercel.app",
+                    "https://75.119.144.130"
+
         ],
         supports_credentials=True
     )
