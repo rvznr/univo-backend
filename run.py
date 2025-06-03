c79b8254b94dea1a13313663468796865ce7206c
Ravza <ravzasisiknur@gmail.com>
2025-06-04 02:31:51 +0300

    .

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from app import create_app
 
-print("🔁 Flask app başlatılıyor...")  # DEBUG
+print("📦 run.py başladı...")
 
 app = create_app()
 
 if __name__ == "__main__":
-    print("🚀 Flask sunucu çalışıyor...")  # DEBUG
+    print("🔥 Flask sunucusu başlatılıyor...")
     app.run(host="0.0.0.0", port=5000)
