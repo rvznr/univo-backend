9b3e6c4ce0f71e3684c9849f0505475ec2c88dc6
Ravza <ravzasisiknur@gmail.com>
2025-06-04 12:04:55 +0300

    3

--- app/__init__.py
+++ app/__init__.py
@@ -27,9 +27,7 @@ def create_app():
     CORS(
         app,
         resources={r"/*": {"origins": [
-            "https://univo-frontend.vercel.app",
-            "https://univoxacademia.online",
-            "https://www.univoxacademia.online"
+            "https://univo-frontend.vercel.app"
         ]}},
         supports_credentials=True
 )
